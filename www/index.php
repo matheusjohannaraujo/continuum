c2971a1b9db821340a7e1d83c4859e9fcf278994
Matheus Johann Araújo <matheus.araujo@recife.pe.gov.br>
2023-08-02 13:49:33 -0300

    Update php.ini, index.php and docker-compose.yml

--- www/index.php
+++ www/index.php
@@ -11,6 +11,9 @@ function config_init()
 	ini_set("max_execution_time", "3600");
 	ini_set("default_socket_timeout", "3600");
 	ini_set("memory_limit", "6144M");
+	// Errors
+	ini_set("error_reporting", 0);
+    //ini_set("display_errors", 0);
 }
 
 config_init();
