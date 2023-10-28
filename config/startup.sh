6d3a6021cd91e878ea9cae6a9123dc5f76cf5202
Matheus Johann Araújo <matheus.araujo@recife.pe.gov.br>
2023-10-28 13:18:49 -0300

    Update startup.sh

--- config/startup.sh
+++ config/startup.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 /usr/sbin/cron -L 8 &
 
+# allocate swap space
+fallocate -l 256M /swapfile
+chmod 0600 /swapfile
+mkswap /swapfile
+echo 10 > /proc/sys/vm/swappiness
+swapon /swapfile
+echo 1 > /proc/sys/vm/overcommit_memory
+
 if [ -f /var/www/html/task.cron ]; then
    tasks=$(cat /var/www/html/task.cron);
    if [ ! -z "$tasks" ]; then
