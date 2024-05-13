8a25cbe0173d1e3888fa2e6e47204e0cb313497e
Matheus Johann Araújo <47996428+matheusjohannaraujo@users.noreply.github.com>
2024-05-12 22:58:31 -0300

    Update AES_256.php

--- www/lib/AES_256.php
+++ www/lib/AES_256.php
@@ -5,7 +5,7 @@
 	Country: Brasil
 	State: Pernambuco
 	Developer: Matheus Johann Araujo
-	Date: 2020-12-31
+	Date: 2024-05-12
 */
 
 namespace Lib;
@@ -13,41 +13,41 @@ namespace Lib;
 class AES_256 {
 
     // The key defined here will be used in the AES 256 CBC and AES 256 GCM methods
-    private $key;
+
+    private $key = "AES_256";
 
     public function __construct()
     {
         $this->key = input_env("AES_256_SECRET", "AES_256");
     }
 
-    public function setKey(string $key) :void
+    public function setKey(string $key)
     {
         $this->key = $key;
+        return $this;
     }
 
-    public function getKey() :string
+    public function getKey()
     {
         return $this->key;
     }  
 
     // Below are the methods for working with AES 256 with CBC
 
-    private function enc_cbc(string &$text, string $key) :string
-    {
+    private function enc_cbc(string &$text, string $key) {
         $key = substr(hash('sha256', $key, true), 0, 32);
         $cipher = 'aes-256-cbc';
         $iv_len = openssl_cipher_iv_length($cipher);
         $iv = openssl_random_pseudo_bytes($iv_len);
         $text = openssl_encrypt(base64_encode($text), $cipher, $key, OPENSSL_RAW_DATA, $iv);
-        return bin2hex(base64_encode($iv . $text));
+        return base64_encode($iv . $text);
     }
     
-    private function dec_cbc(string &$text, string $key) :string
-    {
+    private function dec_cbc(string &$text, string $key) {
         $key = substr(hash('sha256', $key, true), 0, 32);
         $cipher = 'aes-256-cbc';
         $iv_len = openssl_cipher_iv_length($cipher);
-        $text = base64_decode(hex2bin($text));
+        $text = base64_decode($text);
         $iv = substr($text, 0, $iv_len);
         $text = substr($text, $iv_len);    
         return base64_decode(openssl_decrypt($text, $cipher, $key, OPENSSL_RAW_DATA, $iv));
@@ -65,25 +65,26 @@ class AES_256 {
  
     // Below are the methods for working with AES 256 with GCM
 
-    private function enc_gcm(string &$text, string $key, string &$tag) :string
-    {
+    private function enc_gcm(string &$text, string $key, string &$tag) {
         $key = substr(hash('sha256', $key, true), 0, 32);
         $cipher = 'aes-256-gcm';
         $iv_len = openssl_cipher_iv_length($cipher);
         $iv = openssl_random_pseudo_bytes($iv_len);
         $tag_length = 16;
         $text = openssl_encrypt(base64_encode($text), $cipher, $key, OPENSSL_RAW_DATA, $iv, $tag, "", $tag_length);
-        return bin2hex(base64_encode($iv . $tag. $text));
+        return base64_encode($iv . $tag. $text);
     }
     
-    private function dec_gcm(string &$text, string $key, string $tag) :string
-    {
-        $tag = hex2bin($tag);
+    private function dec_gcm(string &$text, string $key, string $tag) {
+        if (empty($tag)) {
+            return null;
+        }
+        $tag = base64_decode($tag);
         $key = substr(hash('sha256', $key, true), 0, 32);
         $cipher = 'aes-256-gcm';
         $iv_len = openssl_cipher_iv_length($cipher);
         $tag_length = 16;
-        $text = base64_decode(hex2bin($text));
+        $text = base64_decode($text);
         $iv = substr($text, 0, $iv_len);
         $tag = substr($text, $iv_len, $tag_length);
         $text = substr($text, $iv_len + $tag_length);    
@@ -93,7 +94,7 @@ class AES_256 {
     public function encrypt_gcm(string $text, string &$tag) :string
     {
         $text = $this->enc_gcm($text, $this->key, $tag);
-        $tag = bin2hex($tag);
+        $tag = base64_encode($tag);
         return $text;
     }
 
